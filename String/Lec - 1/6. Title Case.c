1106dc7cc80a526da6b9d052409cfc7bd836b8db
sajid shaikh <149478269+mrsajidshaikh@users.noreply.github.com>
2023-12-13 20:29:19 +0530

    Add files via upload

---
+++ String/Lec - 1/6. Title Case.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char a[100];
+	
+	printf("Enter your name := ");
+	gets(a);
+	
+	int i;
+	
+	
+	for(i=1;a[i];i++)
+	{	
+		if(a[i]>=67 && a[i]<=97)
+		{
+			a[i] = a[i]+32;
+		} 
+		else if(a[i]==' ')
+		{
+			a[i]=a[i]-32;
+		}
+	}
+
+	if(a[0]>=97 && a[0]<=122)
+	{
+		a[i] = a[i]+32;
+	}
+	 puts(a);
+	 
+}
