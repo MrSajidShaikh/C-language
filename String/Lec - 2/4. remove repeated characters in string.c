af39638a8580e898758179298c6051d9bd2a9fc8
sajid shaikh <149478269+mrsajidshaikh@users.noreply.github.com>
2023-12-14 15:32:20 +0530

    Add files via upload

---
+++ String/Lec - 2/4. remove repeated characters in string.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+	char a[100];
+	
+	printf("Enter the string := ");
+	gets(a);
+	
+	int length = strlen(a);
+	int i,j,f=1;
+	
+	for(i=0;i<length;i++)
+	{
+		for(j=i+1;j<length;j++)
+		{
+			if(a[i]==a[j])
+			{
+				f++;
+				a[j]=0;
+			}
+		}
+		if(a[i]!=0)
+		{
+			printf("%c",a[i]);
+		}
+		f=1;
+	}
+	
+	
+	
+	
+	
+}
