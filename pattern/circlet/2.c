9894bd460f0a9fcbbb4943024803790a3418fe63
sajid shaikh <149478269+mrsajidshaikh@users.noreply.github.com>
2023-12-15 15:53:50 +0530

    Add files via upload

---
+++ pattern/circlet/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j,num=11;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",num);
+			num++;
+		}
+			printf("\n");
+	}
+}
