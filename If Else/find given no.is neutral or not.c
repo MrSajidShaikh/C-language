c84d5cd034e682019d966f911579ff0f4c14a5a9
sajid shaikh <149478269+mrsajidshaikh@users.noreply.github.com>
2023-12-17 22:23:00 +0530

    Add files via upload

---
+++ If Else/find given no.is neutral or not.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a;
+	
+	printf("enter any value:");
+	scanf("%d",&a);
+	
+	if(a==0)
+	{
+		printf("'%d'it is a neutral number",a);
+	}
+	else
+	{
+		if(a>0)
+		{
+			printf("it's not a neutral number.",a);
+		}
+		
+		else
+		{
+			printf("it's not a neutral number.",a);
+		}
+	}
+}
